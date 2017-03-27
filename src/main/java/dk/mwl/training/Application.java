08e56412bd522e9c6cb6fd85f2fd5df75aac6520
mwl <martin@mwl.dk>
2017-03-27 11:56:56 +0100

    Added greeter command line runner

--- src/main/java/dk/mwl/training/Application.java
+++ src/main/java/dk/mwl/training/Application.java
@@ -1,12 +1,20 @@
 package dk.mwl.training;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    @Bean
+    public CommandLineRunner cliRunner(@Value("${greeter.name}") String greeterName) {
+        return args -> System.out.println("Hello " + greeterName);
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
