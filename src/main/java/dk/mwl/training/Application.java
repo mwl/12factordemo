2f084c5ccad3707810fbd7a6aa220c4087548df4
mwl <martin@mwl.dk>
2017-03-27 14:06:32 +0100

    Added RestController

--- src/main/java/dk/mwl/training/Application.java
+++ src/main/java/dk/mwl/training/Application.java
@@ -5,9 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping
+    public String root(@Value("${greeter.name}") String greeterName) {
+        return "Hello " + greeterName;
+    }
 
     @Bean
     public CommandLineRunner cliRunner(@Value("${greeter.name}") String greeterName) {
